3a3ea910d93714d8ae73076c2da49143eb7f7785
Ronald van Kouwen <ronaldvk90@outlook.com>
2026-02-11 20:23:06 +0100

    Nix wants EVERYTHING in git

--- home.nix
+++ home.nix
@@ -18,6 +18,7 @@
     ".config/sway/config".source = ./configs/sway/config;
     ".config/foot/foot.ini".source = ./configs/foot/foot.ini;
     ".config/swaybar/swaybar.sh".source = ./configs/swaybar/swaybar.sh;
+    ".tmux.conf".source = ./configs/tmux.conf;
   };
 
   # Home Manager can also manage your environment variables through
@@ -37,7 +38,39 @@
   #  /etc/profiles/per-user/ronald/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
+  TERM = "xterm";
   };
+
+###########################################
+ programs.git = {
+    package = pkgs.git;
+    enable = true;
+    settings.user.name = "Ronald van Kouwen";
+    settings.user.email = "ronaldvk90@outlook.com";
+
+
+  };
+
+ programs.tmux = {
+    enable = true;
+    shortcut = "a";
+    baseIndex = 1;
+    newSession = true;
+    escapeTime = 0;
+    secureSocket = false;
+    mouse = true;
+    clock24 = true;
+    historyLimit = 50000;
+
+    plugins = with pkgs; [
+      tmuxPlugins.better-mouse-mode
+    ];
+  };
+
+  programs.tmate = {
+    enable = true;
+  };
+###############################################
 
   nixpkgs.config.allowUnfree = true;
   programs.home-manager.enable = true;
